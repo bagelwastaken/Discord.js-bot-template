e6033cfe178fd5e4ae78b39d042fc95d702adee5
Mathusan.P <78277278+bagelwastaken@users.noreply.github.com>
2021-07-19 17:07:03 +0100

    Updated index.js!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //make sure to install discord in the terminal by doing npm i discord.js
 const discord = require('discord.js')
 const bot = new Discord.Client()
-const {prefix,token} = require("./botconfig.json");
+const prefix = !
+const token = {Your token goes here}
 bot.commands = new Discord.Collection();
-
 
 bot.on("ready", () => {
     console.log(`bot has started, with ${bot.users.cache.size} users, in ${bot.channels.cache.size} channels of ${bot.guilds.cache.size} guilds.`);
